3fb492e633c2aea39f42aee0d9873b34740a3033
CrystalC20 <59667034+CrystalC20@users.noreply.github.com>
2020-02-09 17:02:01 -0500

    More EDA

--- EDA.R
+++ EDA.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-d = read.csv("preprocessed.csv")
+d = read.csv("combined.csv")
 d2 = read.csv("diabetic_data2.csv")
 
 # looking at race in relation to re-admission
@@ -37,3 +37,45 @@ ggplot(data = d, aes(x = time_in_hospital)) +
 ggplot(data = d, aes(x = A1Cresult, y = readmitted_.30)) + 
   geom_bar(stat = "identity") 
 d %>% group_by(A1Cresult) %>% summarize(total = sum(readmitted_.30))  
+
+# diagnostic 1
+diag1 = d %>% select(diag_1, readmitted_.30, readmitted_.30.1, readmitted_NO)
+colnames(diag1) = c("diag_1", "RA_under_30", "RA_over_30", "RA_No")
+diag1 = diag1 %>% 
+  pivot_longer(RA_under_30:RA_No, names_to = "Readmittance_Type", values_to = "Value")
+prop_diag1 = diag1 %>% 
+  group_by(diag_1, Readmittance_Type) %>% 
+  summarize(n = sum(Value)) %>% 
+  group_by(diag_1) %>% 
+  mutate(prop = n/sum(n))
+
+diag1 %>% group_by(Readmittance_Type) %>% summarize(count = sum(Value))
+levels(diag1$diag_1) = c("Others", "428")
+ggplot(data = prop_diag1, aes(x = diag_1, y = prop, fill = Readmittance_Type)) +
+  geom_bar(stat = "identity") + 
+  scale_fill_manual(labels = c("Not readmitted", ">30 days", "<30 days"), 
+                    values = c("lightpink1", "lightpink3", "lightpink4")) + 
+  ggtitle("Readmittance Proportions for Most Frequent IDs in Diag 1") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) + 
+  labs(fill = "Readmittance Type", x = "Diagnostic 1 IDs", y = "Proportion")
+
+# diagnostic 2
+diag1 = d %>% select(diag_1, readmitted_.30, readmitted_.30.1, readmitted_NO)
+colnames(diag1) = c("diag_1", "RA_under_30", "RA_over_30", "RA_No")
+diag1 = diag1 %>% 
+  pivot_longer(RA_under_30:RA_No, names_to = "Readmittance_Type", values_to = "Value")
+prop_diag1 = diag1 %>% 
+  group_by(diag_1, Readmittance_Type) %>% 
+  summarize(n = sum(Value)) %>% 
+  group_by(diag_1) %>% 
+  mutate(prop = n/sum(n))
+
+diag1 %>% group_by(Readmittance_Type) %>% summarize(count = sum(Value))
+levels(diag1$diag_1) = c("Others", "428")
+ggplot(data = prop_diag1, aes(x = diag_1, y = prop, fill = Readmittance_Type)) +
+  geom_bar(stat = "identity") + 
+  scale_fill_manual(labels = c("Not readmitted", ">30 days", "<30 days"), 
+                    values = c("lightpink1", "lightpink3", "lightpink4")) + 
+  ggtitle("Readmittance Proportions for Most Frequent IDs in Diag 1") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) + 
+  labs(fill = "Readmittance Type", x = "Diagnostic 1 IDs", y = "Proportion")
